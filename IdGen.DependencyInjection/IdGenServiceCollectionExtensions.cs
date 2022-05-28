c4eee959dddcf140a3af8b6646c304ea190d3757
RobThree <rob@devcorner.nl>
2022-05-28 16:24:14 +0200

    Updated documentation

--- IdGen.DependencyInjection/IdGenServiceCollectionExtensions.cs
+++ IdGen.DependencyInjection/IdGenServiceCollectionExtensions.cs
@@ -4,11 +4,28 @@ using System;
 
 namespace IdGen.DependencyInjection
 {
+    /// <summary>
+    /// Helper class to integrate IdGen with Microsoft.Extensions.DependencyInjection
+    /// </summary>
     public static class IdGenServiceCollectionExtensions
     {
+        /// <summary>
+        /// Registers a singleton <see cref="IdGenerator"/> with the given <paramref name="generatorId"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to register the singleton <see cref="IdGenerator"/> on.</param>
+        /// <param name="generatorId">The generator-id to use for the singleton <see cref="IdGenerator"/>.</param>
+        /// <returns>The given <see cref="IServiceCollection"/> with the registered singleton in it.</returns>
         public static IServiceCollection AddIdGen(this IServiceCollection services, int generatorId)
             => AddIdGen(services, generatorId, () => IdGeneratorOptions.Default);
 
+        /// <summary>
+        /// Registers a singleton <see cref="IdGenerator"/> with the given <paramref name="generatorId"/> and <see cref="IdGeneratorOptions"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to register the singleton <see cref="IdGenerator"/> on.</param>
+        /// <param name="generatorId">The generator-id to use for the singleton <see cref="IdGenerator"/>.</param>
+        /// <param name="options">The <see cref="IdGeneratorOptions"/> for the singleton <see cref="IdGenerator"/>.</param>
+        /// <returns>The given <see cref="IServiceCollection"/> with the registered singleton <see cref="IdGenerator"/> in it.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="options"/> is null</exception>
         public static IServiceCollection AddIdGen(this IServiceCollection services, int generatorId, Func<IdGeneratorOptions> options)
         {
             if (options == null)
